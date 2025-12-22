aa00e689cfdcd57aad5a89c43d7257fa0d802837
silverfish000 <245870771+silverfish000@users.noreply.github.com>
2025-12-23 00:10:01 +0100

    Modifications pour les petits patchs

--- utils/main.py
+++ utils/main.py
@@ -20,7 +20,7 @@ dictionnaire_auto = {
 def clear() :
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def admin_choix(categorie, categorie_auto) : # pour plus tard
+def admin_choix(categorie, categorie_auto) :
     clear()
     print("="*70)
     print(" CONFIGURATION ADMIN ".center(70, "═"))
@@ -34,7 +34,6 @@ def admin_choix(categorie, categorie_auto) : # pour plus tard
         print("-" * 30)
         choix_admin_categories = input("Ecris ton choix -->")
         clear()
-    # Verification du choix ADMIN
         if (choix_admin_categories == 'AUTO') :
             print("Tu as choisis la fonction 'auto' le systeme va le mettre en place")
             categorie_final = categorie_auto
@@ -58,10 +57,19 @@ def admin_choix(categorie, categorie_auto) : # pour plus tard
     time.sleep(3)
     return categorie_final
 
+def add_mdp(choix_user) : # a faire plus tard 
+    if (choix_user == 1) :
+        input("Choisis")
+    elif (choix_user == 2) :
+        print("Tu as choisis l'option 2")
+    else :
+        print("Tu as choisis l'option 3")
 
-admin_choix(dictionnaire_auto, dictionnaire_auto)
+def supp_mdp(choix_user) :
+    print(choix_user)
 
-def aff_menu() :
+
+def aff_menu() : # a ameliorer
     clear()
     print("-="*35)
     print("GESTIONNAIRE DE MOT DE PASSE".center(65))
@@ -70,6 +78,7 @@ def aff_menu() :
     print("1) Ajouter un mot de passe\n")
     print("2) Modifier un mot de passe\n")
     print("3) Supprimer un mot de passe\n")
+    print("4) Admin Choix (ONLY ADMIN)")
     while (True) :
         try :
             choix_user = int(input("Fais ton choix (QUE DES CHIFFRES) :"))
@@ -78,24 +87,24 @@ def aff_menu() :
             print("ERREUR : Tu ne dois que choisir des nombres")
     if (choix_user == 1) :
         print("Tu as choisis l'option 1")
+        # Ajouter le systeme add_mdp
     elif (choix_user == 2) :
         print("Tu as choisis l'option 2")
-    else :
+        # Ajouter le systeme modif_mdp
+    elif (choix_user == 3) :
         print("Tu as choisis l'option 3")
+        # Ajouter le systeme supp_mdp
+    else :
+        print("Tu as choisis l'option 4")
+        admin_choix(dictionnaire_auto, dictionnaire_auto)
     return choix_user
 
 
 
-def add_mdp(choix_user) :
-    if (choix_user == 1) :
-        input("Choisis")
-    elif (choix_user == 2) :
-        print("Tu as choisis l'option 2")
-    else :
-        print("Tu as choisis l'option 3")
+
 choix = aff_menu()
 
-add_mdp(choix)
+aff_menu()
 
 
 # _0_
