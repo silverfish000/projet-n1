7017bec39783bde74ef3f4611f6134346655c983
silverfish000 <245870771+silverfish000@users.noreply.github.com>
2025-12-22 23:28:48 +0100

    Ajout de la logique de choix de categories (utils/main.py) dans la fonction admin_choix

--- utils/main.py
+++ utils/main.py
@@ -1,10 +1,11 @@
 # Ajouter/Modifier/Supprimer des mots de passe
 import os
+import time
 
 
 # ------------ db de categories et tout les mots de passes (non chiffres) ------------
 dictionnaire_test = {
-    'categories_mot_de_passe' : ""
+    'categories_mot_de_passe' : ["Netflix", "Amazon", "Github"]
 }
 # ------------ FIN ------------
 
@@ -12,13 +13,34 @@ dictionnaire_test = {
 def clear() :
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def admin_choix() : # pour plus tard
+def admin_choix(categories) : # pour plus tard
     clear()
     print("-="*35)
     print("GESTIONNAIRE DES CHOIX UTILISATEURS (ONLY ADMIN)".center(65))
     print("-="*35)
-    print()
+    print("Avant de commencer ton expérience ...")
+    while (True) :
+            choix_admin_categories = input("Choisis une catégorie à ajouter pour les users (ex : Netflix, Amazon, Github, Apple)\n" + "Ou tape 'auto' pour utiliser les catégories par défaut : ")
+            clear()
+        # Verification du choix ADMIN
+            if (choix_admin_categories == 'auto') :
+                print("Tu as choisis la fonction 'auto' le systeme va le mettre en place")
+                # Systeme auto declenchement
+                time.sleep(2)
+            else :
+                print("Pour arreter : tape 'STOP'\n")
+                if (choix_admin_categories == 'STOP') :
+                    break
+                elif (choix_admin_categories in categories['categories_mot_de_passe']):
+                            clear()
+                            print(f"La categorie '{choix_admin_categories}' existe deja\n")
+                else :
+                    categories['categories_mot_de_passe'].append(choix_admin_categories)
+                    clear()
+                    print(f"La categorie '{choix_admin_categories}' a bien etait rajoutee !\n")
 
+
+admin_choix(dictionnaire_test)
 
 def aff_menu() :
     clear()
